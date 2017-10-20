ce5501d5a7f519d1c988d911d34dad6827b9fc0a
Chris Frashure <csfrashure@gmail.com>
2017-10-20 16:17:39 -0400

    Updated costs

--- Project.java
+++ Project.java
@@ -1,8 +1,12 @@
-public class Project{
+public class Project {
   public static void main(String[] args) {
 
     double laborHours;
     double laborDollars;
+    int weekHours;
+    double utilities;
+    double rent;
+    double supplies;
 
   }
 }
